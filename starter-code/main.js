0bcac80744de019eb51ebcf9067232af7057a855
Kylan Ishii <kylanishii@gmail.com>
2017-02-28 18:59:01 -0800

    add if else

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,13 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if(cardTwo===cardFour) {
+  alert("Sorry, try again.");
+} else if(cardTwo===cardOne) {
+  alert("You found a match!");
+} else if(cardThree===cardFour) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
